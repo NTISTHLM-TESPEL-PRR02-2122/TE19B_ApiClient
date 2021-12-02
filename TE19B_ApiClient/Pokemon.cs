78711f57b3651d9146f4437bb9a66c3de14f57bd
Mikael Bergström <krank23@gmail.com>
2021-12-02 11:18:40 +0100

    Initial commit

---
+++ TE19B_ApiClient/Pokemon.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Pokemon
+{
+  public string name {get; set;}
+  public int weight {get; set;}
+  public bool is_default {get; set;}
+}
---
+++ TE19B_ApiClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using RestSharp;
+
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+RestRequest pokeRequest = new RestRequest("pokemon/genesect");
+IRestResponse pokeResponse = pokeClient.Get(pokeRequest);
+
+Console.WriteLine(pokeResponse.Content);
+
+Console.ReadLine();
