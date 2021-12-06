6c74527523e4440e6de3636134f6adc1e9bbeafc
Mikael Bergström <krank23@gmail.com>
2021-12-06 13:16:37 +0100

    Swapi och deserialisering

---
+++ TE19B_ApiClient/Person.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Person
+{
+  // 	"name": "Luke Skywalker",
+	// "height": "172",
+	// "mass": "77",
+
+  public string name {get; set;}
+  public string height {get; set;}
+  public string mass {get; set;}
+}
--- TE19B_ApiClient/Program.cs
+++ TE19B_ApiClient/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Text.Json;
 using RestSharp;
-
 
 RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
 RestRequest pokeRequest = new RestRequest("pokemon/genesect");
 IRestResponse pokeResponse = pokeClient.Get(pokeRequest);
+// Console.WriteLine(pokeResponse.Content);
 
-Console.WriteLine(pokeResponse.Content);
+Pokemon p = JsonSerializer.Deserialize<Pokemon>(pokeResponse.Content);
+
+Console.WriteLine(p.name);
+
+
+// RestClient swClient = new RestClient("https://swapi.dev/api/");
+// RestRequest swRequest = new RestRequest("people/50");
+// IRestResponse swResponse = swClient.Get(swRequest);
+// Console.WriteLine(swResponse.Content);
+
+// Person p = JsonSerializer.Deserialize<Person>(swResponse.Content);
+
+// Console.WriteLine(p.name);
 
 Console.ReadLine();
